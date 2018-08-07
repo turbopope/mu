dfb752624cd801749747ac6a22cc7537dfc4f78d
Marco Brack <m.brack@mygcs.de>
2018-08-07 13:54:00 +0200

    Give every food a slider

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -5,6 +5,7 @@ import { tap, startWith, map } from 'rxjs/operators';
 import { FormControl } from '@angular/forms';
 import { combineLatest } from 'rxjs';
 import * as math from 'mathjs';
+import { set } from 'lodash';
 
 @Component({
   selector: 'page-home',
@@ -12,18 +13,15 @@ import * as math from 'mathjs';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController, private foodProvider: FoodProvider) {
+  public FOOD_KEYS = ['currants', /*'oats',*/ 'apples', 'chia_seeds', 'cashews', 'rice_milk'];
 
+  constructor(public navCtrl: NavController, private foodProvider: FoodProvider) {
   }
 
-  public foods = combineLatest(
-    this.foodProvider.getFood('currants'),
-    // this.foodProvider.getFood('oats'),
-    this.foodProvider.getFood('apples'),
-    this.foodProvider.getFood('chia_seeds'),
-    this.foodProvider.getFood('cashews'),
-    this.foodProvider.getFood('rice_milk'),
-  );
+  // public foods = combineLatest(this.FOOD_KEYS.map(this.foodProvider.getFood));
+  public foods = this.FOOD_KEYS.reduce((foods, foodKey) => set(foods, foodKey, this.foodProvider.getFood(foodKey)), {});
+  public amountControllers = this.FOOD_KEYS.reduce((controllers, foodKey) => set(controllers, foodKey, new FormControl()), {});
+  public amounts = this.FOOD_KEYS.reduce((amounts, foodKey) => set(amounts, foodKey, this.amountControllers[foodKey].valueChanges.pipe(startWith(0))), {});
 
   public food = this.foodProvider.getFood('currants');
   
