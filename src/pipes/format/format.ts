31645c01a49c7d2a6b2cc07b8b802e789fbbc824
Marco Brack <m.brack@mygcs.de>
2018-08-07 23:15:46 +0200

    Implement pipe for formatting math.js units

--- src/pipes/format/format.ts
+++ src/pipes/format/format.ts
@@ -1,18 +1,11 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { Unit, format, FormatOptions } from 'mathjs';
 
-/**
- * Generated class for the FormatPipe pipe.
- *
- * See https://angular.io/api/core/Pipe for more info on Angular Pipes.
- */
 @Pipe({
   name: 'format',
 })
 export class FormatPipe implements PipeTransform {
-  /**
-   * Takes a value and makes it lowercase.
-   */
-  transform(value: string, ...args) {
-    return value.toLowerCase();
+  transform(unit: Unit, options: number|FormatOptions = 2) {
+    return format(unit, options);
   }
 }
