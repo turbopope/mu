7fab3dfd7d34e1a4bf54934a0152f42a9b42ce26
Marco Brack <m.brack@mygcs.de>
2018-08-07 22:44:18 +0200

    Generate FormatPipe

---
+++ src/pipes/pipes.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { FormatPipe } from './format/format';
+@NgModule({
+	declarations: [FormatPipe],
+	imports: [],
+	exports: [FormatPipe]
+})
+export class PipesModule {}
