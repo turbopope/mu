3e85dc5b33b8c27dfbbc43ea1a83b2ac881e2fd5
Marco Brack <m.brack@mygcs.de>
2018-08-25 11:24:24 +0200

    Add a persistent slider for a goal

--- src/pages/goals/goals.ts
+++ src/pages/goals/goals.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { Storage } from '@ionic/storage';
+import { FormControl } from '@angular/forms';
+import { startWith, debounceTime } from 'rxjs/operators';
+import { from } from 'rxjs';
 
 /**
  * Generated class for the GoalsPage page.
@@ -16,11 +19,12 @@ import { Storage } from '@ionic/storage';
 })
 export class GoalsPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private storage: Storage) {
-  }
+  public control = new FormControl();
+  public amount = this.control.valueChanges;
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad GoalsPage');
+  constructor(public navCtrl: NavController, public navParams: NavParams, private storage: Storage) {
+    from(this.storage.get('amount')).subscribe(amount => this.control.setValue(amount || 11));
+    this.control.valueChanges.pipe(debounceTime(100)).subscribe(changed => storage.set('amount', changed))
   }
 
 }
